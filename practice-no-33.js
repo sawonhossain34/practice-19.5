3638ce985670dbd9105bd9979890bc0b4bd37c02
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-29 16:12:00 +0600

    while and for loop array output file add

---
+++ practice-no-33.js
@@ -0,0 +1,10 @@
+ var skills = ['html', 'css', 'bootstrap', 'tailwind', 'javascript'];
+var i = 0;
+  for(i = 0 ; i < skills.length; i++){
+     console.log(skills[i]);
+  }
+// var i = 0;
+//  while(i < skills.length){
+//     console.log(skills[i])
+//     i++;
+//  }
