510df4533ae3b659e08f732a1b273e10c0f9a9ed
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-29 14:56:54 +0600

    while and for loop 1-39serial show

---
+++ practice-no-25.js
@@ -0,0 +1,14 @@
+//   While loop 
+  var sad = 0;
+
+ while(sad < 39 ){
+    //  console.log('ajke amr mon vlo nei')
+     sad++;
+     console.log('sad');
+ }
+
+// for loop
+// for(sad = 0 ; sad < 39 ; sad++){
+//     console.log('ajke amr mon vala nei');
+// }
+
