683ef910761e3dcc61ed5535d63264fb4cddf189
sawonhossain34 <sawonhossain3491@gmail.com>
2023-01-29 14:34:25 +0600

    practice-24 complate fiile add

--- practice-no-24.js
+++ practice-no-24.js
@@ -1,3 +1,14 @@
-var money = 80000;
+var money = 10001;
 
-if
+if(money > 80000){
+    console.log('ami mac kinbo');
+}
+else if(money > 60000){
+    console.log('ami gaming laptop kinbo');
+}else if(money > 40000){
+    console.log('ami lenovo computer kinbo');
+}else if(money > 20000){
+    console.log('ami old laptop kinbo');
+}else{
+    console.log('mobail diye kaj chalabo')
+}
